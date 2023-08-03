fa8046a39b46b3f0c9d613a6ae90779932730c51
VitaliiPshenychnyi <117928970+vitaliipshenychnyi@users.noreply.github.com>
2023-08-03 15:10:43 +0300

    add validate name, email and phone

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -1,9 +1,11 @@
 const Joi = require("joi");
 
 const addSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
+  name: Joi.string().alphanum().min(3).max(30).required(),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+    .required(),
+  phone: Joi.string().pattern(/^\d+$/).required(),
 });
 
 module.exports = {
