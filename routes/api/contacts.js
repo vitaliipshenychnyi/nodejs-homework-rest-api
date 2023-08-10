c981fb1d2372d7dbedb0c72a65afac3a27e520b6
VitaliiPshenychniy <4kvaap@gmail.com>
2023-08-10 13:01:36 +0300

    add get

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -7,12 +7,12 @@ const schemas = require("../../schemas/contacts");
 
 router.get("/", ctrl.getAll);
 
-router.get("/:contactId", ctrl.getById);
+// router.get("/:contactId", ctrl.getById);
 
-router.post("/", validateBody(schemas.addSchema), ctrl.add);
+// router.post("/", validateBody(schemas.addSchema), ctrl.add);
 
-router.put("/:contactId", validateBody(schemas.addSchema), ctrl.updateById);
+// router.put("/:contactId", validateBody(schemas.addSchema), ctrl.updateById);
 
-router.delete("/:contactId", ctrl.deleteById);
+// router.delete("/:contactId", ctrl.deleteById);
 
 module.exports = router;
